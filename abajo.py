7ba99b680c664e39354d822ddc720e6a7919504e
giordanyepesruiz0802 <161129810+giordanyepesruiz0802@users.noreply.github.com>
2024-05-08 15:56:29 -0500

    Create abajo.py

---
+++ abajo.py
@@ -0,0 +1,19 @@
+import dash
+import dash_bootstrap_components as dbc 
+from dash import html, dcc, Input, Output, Dash
+import pandas as pd
+from difflib import get_close_matches
+
+
+tabla_resultados = dbc.Container([
+    html.H6("Resultados de la Comparación", className='mb-4 text-center'),
+    dash_table.DataTable(
+        id='tabla_resultados',
+        columns=[
+            {'name': 'Item Similar', 'id': 'ITEM'},
+            {'name': 'Valor Total', 'id': 'TOTAL'},
+            {'name': 'Valor Menor', 'id': 'VALOR_MENOR'}
+        ],
+        style_table={'overflowX': 'auto'}
+    ),
+], className='shadow p-3 border rounded')
