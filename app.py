0f276c457673659b339e87f2dad3231a481e11a6
giordanyepesruiz0802 <161129810+giordanyepesruiz0802@users.noreply.github.com>
2024-05-08 19:33:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import dash
 import dash_bootstrap_components as dbc 
-from dash import html, Dash, dcc, Input, Output
+from dash import html, dcc, Input, Output
 import pandas as pd 
 from frontend.layout import layout
 from backend.valores import max_values, item_1, item_2, valor_1, valor_2
 from difflib import get_close_matches
 import dash_table
-
-
 
 # Leer el archivo CSV
 data_csv = pd.read_csv('backend/Lista_oficial_de_precios_unitarios_fijos_de_Obra_P_blica_y_de_consultor_a_-_DEPARTAMENTO_DE_BOYAC__20240505.csv')
@@ -17,7 +15,7 @@ map_valor_1 = dict(zip(item_1['ITEM_1'], valor_1['PRECIO_UNITARIO_1']))
 map_valor_2 = dict(zip(item_2['ITEM_2'], valor_2['PRECIO_UNITARIO_2']))
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
-
+app.layout = layout
 @app.callback(
     Output('valor-menor-container', 'children'),
     [Input('tabla_valor_1', 'data'),
@@ -40,22 +38,19 @@ def update_valor_menor(data_valor_1, data_valor_2):
     Output('VALOR_MENOR', 'data'),
     [Input('valor-menor-container', 'children')]
 )
-nueva_tabla = html.Div([
-    html.H6("Nombres Similares y sus Valores Correspondientes", className='mb-4 text-center'),
+# Añade la tabla VALOR MENOR al layout
+layout.append(html.Div([
+    html.H6("VALOR MENOR", className='mb-4 text-center'),
     dash_table.DataTable(
-        id='nueva_tabla',
+        id='VALOR_MENOR',
         columns=[
-            {'name': 'ITEM', 'id': 'ITEM'},
-            {'name': 'TOTAL', 'id': 'TOTAL'},
-            {'name': 'VALOR MENOR', 'id': 'VALOR_MENOR'}
+            {'name': 'Valor Menor', 'id': 'VALOR_MENOR', 'editable': False},
         ],
+        data=max_values.to_dict('records'),
         style_table={'overflowX': 'auto'}
     )
-])
+]))
 
-
-# Añade la nueva tabla al layout
-app.layout = layout + nueva_tabla
 
 # Callback para actualizar la nueva tabla
 @app.callback(
@@ -92,15 +87,7 @@ def update_similares_table(_):
     similar_data = [{'ITEM': item, 'TOTAL': total, 'VALOR_MENOR': valor_menor} for item, total in zip(similar_items, valores_menor)]
     
     return similar_data
-#Con esto, la nueva tabla tabla_similares se actualizará cada vez que cambie el valor en valor-menor-container, mostrando los nombres similares encontrados en la columna "ITEM", su correspondiente valor de la columna "TOTAL", y el valor menor de la tabla VALOR_MENOR. Asegúrate de ajustar el diseño y la posición de la nueva tabla según sea necesario en tu aplicación.
 
-
-
-
-
-
-Message ChatGPT
-
-ChatGPT can make mistakes. Consider checking important information.
 if __name__ == '__main__':
     app.run_server(debug=True, host='0.0.0.0', port=8050)
+
